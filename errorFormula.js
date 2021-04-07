1fd07d1efb23c97f99dbb9bf230e99f0cccce6b9
RiouC <riou.christophe@protonmail.com>
2021-04-07 16:10:03 +0200

    [formula] add possible solutions

--- errorFormula.js
+++ errorFormula.js
@@ -8,3 +8,27 @@
         </mfrac>
     </mrow>
 </math>
+
+
+<span style={{postion: "relative", textAlign: "center"}}>
+        <span style={{position: "absolute", textAlign: "center"}}>1</span>
+        <span style={{position: "absolute", marginTop: "0.3em", marginLeft: "-1.6em"}}>_________</span>
+        <span style={{position: "absolute", marginTop: "1.5em", marginLeft: "-1.3em"}}>1 + e<sup>-(loc)</sup></span>
+        <span style={{position: "absolute", marginTop: "0.7em", marginLeft: "-7em"}}>P(error)</span>
+        <span style={{position: "absolute", marginTop: "0.7em", marginLeft: "-3em"}}>=</span>
+</span>
+
+
+<table>
+    <tbody>
+        <tr>
+            <td rowspan="2" style="padding-right: 0.5em">
+                    P(error) =
+            </td>
+            <td style="border-bottom:solid 1px; text-align: center">1</td>
+        </tr>
+        <tr>
+            <td>1 + e<sup>-(loc)</sup></td>
+        </tr>
+    </tbody>
+</table>
