64467181dcb36332331a12a670508345f145731b
RiouC <riou.christophe@protonmail.com>
2021-04-07 12:52:30 +0200

    [tdd] remove maths bc chromium shit on mathjax

---
+++ errorFormula.js
@@ -0,0 +1,10 @@
+<math>
+    <mrow>
+        <mi>P(error)</mi>
+        <mo> = </mo>
+        <mfrac>
+            <mi>1</mi>
+            <mi><mn>1</mn> <mo>+</mo> <msup><mi>e</mi><mi>-(loc)</mi></msup></mi>
+        </mfrac>
+    </mrow>
+</math>
